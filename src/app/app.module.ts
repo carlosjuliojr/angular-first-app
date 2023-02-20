a5401a9cf5921330b272afd2fbdfe910247e8977
Julio Jaimes <carlosjuliojr@hotmail.com>
2023-02-20 14:36:58 -0300

    bidirectional databinding in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SlidebarComponent } from './slidebar/slidebar.component';
 import { CardComponent } from './card/card.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SlidebarComponent,
-    CardComponent
+    CardComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
